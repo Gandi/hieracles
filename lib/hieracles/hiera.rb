f3bd5482b92b7854ab7ed24d75ecef273f0fa5d2
mose <mose@gandi.net>
2015-09-07 11:13:41 +0800

    prepare more of the hiera class

--- lib/hieracles/hiera.rb
+++ lib/hieracles/hiera.rb
@@ -4,12 +4,15 @@ module Hieracles
 
     def initialize(hierafile)
       raise IOError, "Hierafile #{hierafile} not found." unless File.exist? hierafile
+      @hierafile = hierafile
       @loaded = YAML.load_file(hierafile)
     end
 
     def datadir
       raise TypeError, "Sorry hieracles only knows yaml backend for now." unless @loaded[:yaml]
-      @loaded[:yaml][:datadir]
+      parampath = File.expand_path(File.join('..', @loaded[:yaml][:datadir]), @hierafile)
+      raise IOError, "Params dir not found." unless Dir.exist? parampath
+      parampath
     end
 
   end
--- spec/lib/hiera_spec.rb
+++ spec/lib/hiera_spec.rb
@@ -7,7 +7,6 @@ describe Hieracles::Hiera do
       let(:hierafile) { File.expand_path('../../files/hiera_no.yaml', __FILE__) }
       it 'raises an error' do
         expect { Hieracles::Hiera.new hierafile }.to raise_error(IOError)
-        # expect(Hieracles::Hiera.new hierafile).to raise_error
       end
     end
     context 'hiera file found' do
@@ -15,8 +14,36 @@ describe Hieracles::Hiera do
       let(:hiera) { Hieracles::Hiera.new hierafile }
       it 'load the file' do
         expect(hiera.instance_variable_get :@loaded).to be_a Hash
+        expect(hiera.instance_variable_get :@hierafile).to eq hierafile
       end
     end
+  end
+
+
+  describe '.datadir' do
+    context 'hiera file do not have a yaml backend' do
+      let(:hierafile) { File.expand_path('../../files/hiera_no_yamlbackend.yaml', __FILE__) }
+      let(:hiera) { Hieracles::Hiera.new hierafile }
+      it 'raises an error' do
+        expect { hiera.datadir }.to raise_error(TypeError)
+      end
+    end
+    context 'hiera file has a yaml backend but dir not found' do
+      let(:hierafile) { File.expand_path('../../files/hiera_yamlbackend_notfound.yaml', __FILE__) }
+      let(:hiera) { Hieracles::Hiera.new hierafile }
+      it 'raises an error' do
+        expect { hiera.datadir }.to raise_error(IOError)
+      end
+    end
+    context 'hiera file has a yaml backend' do
+      let(:hierafile) { File.expand_path('../../files/hiera.yaml', __FILE__) }
+      let(:hiera) { Hieracles::Hiera.new hierafile }
+      let(:parampath) { File.expand_path('../params/', hierafile) }
+      it 'returns params path' do
+        expect(hiera.datadir).to eq parampath
+      end
+    end
+  
   end
 
 end
