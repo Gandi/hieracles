f890f0f257eeba15c12b34572c7eebafeff5e5d1
mose <mose@gandi.net>
2015-09-07 10:56:32 +0800

    fix test for error

--- spec/lib/hiera_spec.rb
+++ spec/lib/hiera_spec.rb
@@ -3,14 +3,13 @@ require 'spec_helper'
 describe Hieracles::Hiera do
 
   describe '.new' do
-
     context 'hiera file not found' do
       let(:hierafile) { File.expand_path('../../files/hiera_no.yaml', __FILE__) }
       it 'raises an error' do
-        expect(Hieracles::Hiera.new hierafile).to raise_error
+        expect { Hieracles::Hiera.new hierafile }.to raise_error(IOError)
+        # expect(Hieracles::Hiera.new hierafile).to raise_error
       end
     end
-    
     context 'hiera file found' do
       let(:hierafile) { File.expand_path('../../files/hiera.yaml', __FILE__) }
       let(:hiera) { Hieracles::Hiera.new hierafile }
@@ -18,7 +17,6 @@ describe Hieracles::Hiera do
         expect(hiera.instance_variable_get :@loaded).to be_a Hash
       end
     end
-
   end
 
 end
