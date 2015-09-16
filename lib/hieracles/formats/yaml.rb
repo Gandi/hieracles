7e0a3becaa258c4d1879f6bafc6b69af390a6349
mose <mose@gandi.net>
2015-09-16 13:21:27 +0800

    move commented yaml code to yaml format class

--- lib/hieracles/formats/yaml.rb
+++ lib/hieracles/formats/yaml.rb
@@ -31,6 +31,41 @@ module Hieracles
         @node.modules.to_yaml
       end
 
+
+      def commented_yaml_tree(without_common = true)
+        tree = params_tree(without_common)
+        params = params(without_common)
+        mergetree('', [], tree, params)
+      end
+
+      def mergetree(output, key, leaf, params)
+        indent = '  ' * key.count
+        case leaf.class.name
+        when 'Hash'
+          leaf.each do |k, v|
+            output += "\n" + indent + k + ': '
+            key << k
+            output = mergetree(output, key, v, params)
+            key = []
+          end
+        when 'Array'
+          yaml = leaf.to_yaml[4..-1]
+          aryaml = yaml.each_line.map do |l|
+            indent + l
+          end
+          output += "\n" + indent + "# " + params[key.join('.')][0][:file]
+          output += "\n" + aryaml.join().chomp
+        when 'String'
+          output += leaf
+          if params["#{key.join('.')}"]
+            output += " # " + params[key.join('.')][0][:file]
+          else
+            raise "#{key}"
+          end
+        end
+        output
+      end
+
     end
   end
 end
