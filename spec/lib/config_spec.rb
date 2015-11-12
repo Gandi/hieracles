5d99ecfb7d105dde277b1435c0f4ca6b2132e2e8
mose <mose@mose.com>
2015-11-12 12:20:36 +0800

    fix test

--- spec/lib/config_spec.rb
+++ spec/lib/config_spec.rb
@@ -73,7 +73,7 @@ describe Hieracles::Config do
       let(:expected) { 'Debian' }
       before { Hieracles::Config.load options }
 
-      it { expect(Hieracles::Config.scope['osfamily']).to eq expected }
+      it { expect(Hieracles::Config.scope[:osfamily]).to eq expected }
     end
     context 'with a json file' do
       let(:options) do
@@ -86,7 +86,7 @@ describe Hieracles::Config do
       let(:expected) { 'Debian' }
       before { Hieracles::Config.load options }
 
-      it { expect(Hieracles::Config.scope['osfamily']).to eq expected }
+      it { expect(Hieracles::Config.scope[:osfamily]).to eq expected }
     end
   end
 
