9b8cba107a3f8f310646d5a28c2747c6e11fa94d
mose <mose@mose.com>
2015-11-16 06:27:44 +0800

    minor refactoring

--- lib/hieracles/interpolate.rb
+++ lib/hieracles/interpolate.rb
@@ -4,19 +4,23 @@ module Hieracles
     def parse(data, values, interactive = false)
       data.gsub(/%\{(?:(scope|hiera|literal|alias) *)?([^\}]*)\}/) do |match|
         if interactive && !values[$2.to_sym]
-          puts
-          puts "#{match} is not defined."
-          puts "Is it missing in your ENC source?"
-          puts "Maybe you should define a default value for that scope variable in your config file?"
-          puts "Do you want to provide a temmporary value? [input value]"
-          print "#{$2} = "
-          val = $stdin.gets.chomp
+          val = ask_about($2)
           values[$2.to_sym] = val
           val
         else
           values[$2.to_sym]
         end
       end
+    end
+
+    def ask_about(var)
+      puts
+      puts "'#{var}' is not defined."
+      puts "Is it missing in your ENC source?"
+      puts "Maybe you should define a default value for that scope variable in your config file?"
+      puts "Do you want to provide a temmporary value? [input value]"
+      print "#{var} = "
+      $stdin.gets.chomp
     end
 
   end
