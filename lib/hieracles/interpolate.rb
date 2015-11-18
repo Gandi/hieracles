b2a67df0bcd9d3bdbdb8d1157a111c6419bb14a0
mose <mose@gandi.net>
2015-11-18 10:04:21 +0800

    fix some tests for interpolation

--- lib/hieracles/interpolate.rb
+++ lib/hieracles/interpolate.rb
@@ -5,7 +5,7 @@ module Hieracles
       data.gsub(/%\{(?:(scope|hiera|literal|alias)\(['"])?(?:::)?([^\}"']*)(?:["']\))?\}/) do |match|
         if interactive && !values[$2.to_sym]
           values[$2.to_sym] = ask_about($2)
-          val
+          values[$2.to_sym]
         else
           values[$2.to_sym]
         end
@@ -13,13 +13,29 @@ module Hieracles
     end
 
     def ask_about(var)
-      puts
-      puts "'#{var}' is not defined."
-      puts "Is it missing in your ENC source?"
-      puts "Maybe you should define a default value for that scope variable in your config file?"
-      puts "Do you want to provide a temmporary value? [input value]"
-      print "#{var} = "
-      $stdin.gets.chomp
+      @@output.puts
+      @@output.puts "'#{var}' is not defined."
+      @@output.puts "Is it missing in your ENC source?"
+      @@output.puts "Maybe you should define a default value for that scope variable in your config file?"
+      @@output.puts "Do you want to provide a temmporary value? [input value]"
+      @@output.print "#{var} = "
+      @@input.gets.chomp
+    end
+
+    # makes possible to set input and output
+    def setio(input, output)
+      @@input = input
+      @@output = output
+    end
+
+    # defaults to STDIN
+    private def input
+      @@input ||= STDIN
+    end
+
+    # defaults to STDOUT
+    private def output
+      @@output ||= STDOUT
     end
 
   end
--- spec/lib/interpolate_spec.rb
+++ spec/lib/interpolate_spec.rb
@@ -4,6 +4,43 @@ describe Hieracles::Interpolate do
   include Hieracles::Interpolate
 
   describe ".parse" do
+    let(:input) { StringIO.new }
+    let(:output) { StringIO.new }
+    before { setio input, output }
+
+    context "when in non-interactive mode" do
+      let(:data) { 'something_%{a_var}' }
+      context "and has valid value" do
+        let(:values) { { a_var: 'value' } }
+        let(:expected) { 'something_value' }
+        it { expect(parse data, values).to eq expected }
+      end
+      context "and has invalid value" do
+        let(:values) { { b_var: 'novalue' } }
+        let(:expected) { 'something_' }
+        it { expect(parse data, values).to eq expected }
+      end
+      context "and has valid value with doublecolumn" do
+        let(:data) { 'something_%{::a_var}' }
+        let(:values) { { a_var: 'value' } }
+        let(:expected) { 'something_value' }
+        it { expect(parse data, values).to eq expected }
+      end
+    end
+    context "when in interactive mode" do
+      let(:data) { 'something_%{a_var}' }
+      context "and has valid value" do
+        let(:values) { { a_var: 'value' } }
+        let(:expected) { 'something_value' }
+        it { expect(parse data, values, true).to eq expected }
+      end
+      context "and has invalid value" do
+        let(:values) { { b_var: 'novalue' } }
+        let(:expected) { 'something_value' }
+        before { allow(input).to receive(:gets).and_return 'value' }
+        it { expect(parse data, values, true).to eq expected }
+      end
+    end
   end
 
   describe ".ask_about" do
