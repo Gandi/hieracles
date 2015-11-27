2ccd1c922f388402322fc004bfc690f20847c2ff
mose <mose@mose.com>
2015-11-28 07:47:32 +0800

    some tests

--- spec/lib/puppetdb/apierror_spec.rb
+++ spec/lib/puppetdb/apierror_spec.rb
@@ -2,6 +2,10 @@ require 'spec_helper'
 
 describe Hieracles::Puppetdb::APIError do
 
-  pending '.new'
+  describe '.new' do
+    let(:response) { Object.new }
+    let(:error) { Hieracles::Puppetdb::APIError.new response }
+    it { expect(error.response).to eq response }
+  end
   
 end
