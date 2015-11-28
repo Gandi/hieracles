1ffadea291ec0d6776fa1296126e5037b2c1064f
mose <mose@mose.com>
2015-11-28 19:21:45 +0800

    simple test

--- spec/lib/puppetdb/response_spec.rb
+++ spec/lib/puppetdb/response_spec.rb
@@ -2,6 +2,12 @@ require 'spec_helper'
 
 describe Hieracles::Puppetdb::Response do
 
-  pending '.new'
+  describe '.new' do
+    let(:data) { 'something' }
+    let(:total_records) { 2 }
+    let(:response) { Hieracles::Puppetdb::Response.new(data, total_records) }
+    it { expect(response.data).to eq data }
+    it { expect(response.total_records).to eq total_records }
+  end
 
 end
