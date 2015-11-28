b39ec8bbcf5750ef0a51963abdad18a65ae4557d
mose <mose@mose.com>
2015-11-28 12:37:06 +0800

    add test files for PKI

--- lib/hieracles/puppetdb/fixsslconnectionadapter.rb
+++ lib/hieracles/puppetdb/fixsslconnectionadapter.rb
@@ -4,12 +4,12 @@ module Hieracles
   module Puppetdb
     class FixSSLConnectionAdapter < HTTParty::ConnectionAdapter
       def attach_ssl_certificates(http, options)
-        raise IOError, "Cert file #{options['cert_file']} not found." unless File.exist? options['cert_file']
-        raise IOError, "Key file #{options['key_file']} not found." unless File.exist? options['key_file']
-        raise IOError, "CA file #{options['ca_file']} not found." unless File.exist? options['ca_file']
+        raise(IOError, "Cert file #{options['cert_file']} not found.") unless File.exist? options['cert_file'].to_s
+        raise(IOError, "Key file #{options['key_file']} not found.") unless File.exist? options['key_file']
+        raise(IOError, "CA file #{options['ca_file']} not found.") unless File.exist? options['ca_file']
         http.cert = OpenSSL::X509::Certificate.new(File.read(options['cert_file']))
-        if options['key+password']
-          http.key = OpenSSL::PKey::RSA.new(File.read(options['key_file']), options['key+password'])
+        if options['key_password']
+          http.key = OpenSSL::PKey::RSA.new(File.read(options['key_file']), options['key_password'])
         else
           http.key = OpenSSL::PKey::RSA.new(File.read(options['key_file']))
         end
--- spec/lib/puppetdb/fixsslconnectionadapter_spec.rb
+++ spec/lib/puppetdb/fixsslconnectionadapter_spec.rb
@@ -2,6 +2,105 @@ require 'spec_helper'
 
 describe Hieracles::Puppetdb::FixSSLConnectionAdapter do
 
-  pending '.attach_ssl_certificates'
+  let(:uri) { URI "https://localhost" }
+  let(:options) { {} }
+  let(:adapter) { Hieracles::Puppetdb::FixSSLConnectionAdapter.new uri, options }
+  describe '.attach_ssl_certificates' do
+
+    context "with cert file" do
+      context "not found" do
+        let(:options) {
+          {
+            'cert_file' => File.expand_path('spec/files/ssl/no-cert.crt'),
+            'key_file' => File.expand_path('spec/files/ssl/key.pem'),
+            'ca_file' => File.expand_path('spec/files/ssl/ca.crt')
+          }
+        }
+        it { expect { adapter.connection }.to raise_error(IOError, /Cert file .*no-cert.crt not found\./) }
+      end
+      context "badly formatted" do
+        let(:options) {
+          {
+            'cert_file' => File.expand_path('spec/files/ssl/bad-cert.crt'),
+            'key_file' => File.expand_path('spec/files/ssl/key.pem'),
+            'ca_file' => File.expand_path('spec/files/ssl/ca.crt')
+          }
+        }
+        it { expect { adapter.connection }.to raise_error(OpenSSL::X509::CertificateError) }
+      end
+    end
+
+    context "with key file" do
+      context "not found" do
+        let(:options) {
+          {
+            'cert_file' => File.expand_path('spec/files/ssl/cert.crt'),
+            'key_file' => File.expand_path('spec/files/ssl/no-key.pem'),
+            'ca_file' => File.expand_path('spec/files/ssl/ca.crt')
+          }
+        }
+        it { expect { adapter.connection }.to raise_error(IOError, /Key file .*no-key.pem not found\./) }
+      end
+      context "badly formatted" do
+        let(:options) {
+          {
+            'cert_file' => File.expand_path('spec/files/ssl/cert.crt'),
+            'key_file' => File.expand_path('spec/files/ssl/bad-key.pem'),
+            'ca_file' => File.expand_path('spec/files/ssl/ca.crt')
+          }
+        }
+        it { expect { adapter.connection }.to raise_error(OpenSSL::PKey::RSAError) }
+      end
+      context "with pasword" do
+        context "correct" do
+          let(:options) {
+            {
+              'cert_file' => File.expand_path('spec/files/ssl/cert.crt'),
+              'key_file' => File.expand_path('spec/files/ssl/key-pass.pem'),
+              'key_password' => 'good-toto',
+              'ca_file' => File.expand_path('spec/files/ssl/ca.crt')
+            }
+          }
+          it { expect { adapter.connection }.not_to raise_error }
+        end
+        context "incorrect" do
+          let(:options) {
+            {
+              'cert_file' => File.expand_path('spec/files/ssl/cert.crt'),
+              'key_file' => File.expand_path('spec/files/ssl/key-pass.pem'),
+              'key_password' => 'bad-toto',
+              'ca_file' => File.expand_path('spec/files/ssl/ca.crt')
+            }
+          }
+          it {expect { adapter.connection }.to raise_error(OpenSSL::PKey::RSAError) }
+        end
+      end
+    end
+
+    context "with CA file" do
+      context "not found" do
+        let(:options) {
+          {
+            'cert_file' => File.expand_path('spec/files/ssl/cert.crt'),
+            'key_file' => File.expand_path('spec/files/ssl/key.pem'),
+            'ca_file' => File.expand_path('spec/files/ssl/no-ca.crt')
+          }
+        }
+        it { expect { adapter.connection }.to raise_error(IOError, /CA file .*no-ca.crt not found\./) }
+      end
+    end
+
+    context "all correct" do
+      let(:options) {
+        {
+          'cert_file' => File.expand_path('spec/files/ssl/cert.crt'),
+          'key_file' => File.expand_path('spec/files/ssl/key.pem'),
+          'ca_file' => File.expand_path('spec/files/ssl/ca.crt')
+        }
+      }
+      it { expect { adapter.connection }.not_to raise_error }
+    end
+
+  end
 
 end
