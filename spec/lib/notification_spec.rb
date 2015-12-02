42f1b8cdefce3c088e0e8b4ef2eaaf63a9d3e2d7
mose <mose@mose.com>
2015-12-03 01:36:56 +0800

    more coverage

--- spec/lib/notification_spec.rb
+++ spec/lib/notification_spec.rb
@@ -3,11 +3,26 @@ require 'spec_helper'
 describe Hieracles::Notification do
 
   describe '.new' do
-
+    let(:notif) { Hieracles::Notification.new 'a_source', 'a_message'}
+    it { expect(notif.level).to eq 'info' }
+    it { expect(notif.message).to eq 'a_message' }
+    it { expect(notif.source).to eq 'a_source' }
   end
 
   describe '.to_hash' do
-
+    let(:notif) { Hieracles::Notification.new 'a_source', 'a_message'}
+    before {
+      allow(Time).to receive(:new).and_return(Time.new(2015))
+    }
+    let(:expected) { 
+      { 
+        'source' => 'a_source',
+        'level' => 'info',
+        'message' => 'a_message',
+        'timestamp' => Time.new(2015)
+      }
+    }
+    it { expect(notif.to_hash).to eq expected }
   end
 
 
