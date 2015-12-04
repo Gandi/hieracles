ded18b0036122d07bfce3cfbfb323a0a744f1eb0
mose <mose@mose.com>
2015-12-04 23:42:42 +0800

    pupdb options

--- lib/hieracles/options/pupdb.rb
+++ lib/hieracles/options/pupdb.rb
@@ -6,53 +6,9 @@ module Hieracles
 
       def available_options
         {
-          config: {
-            has_arg: true,
-            aliases: ['c', 'conf', 'config']
-          },
-          format: {
-            has_arg: true,
-            aliases: ['f', 'format']
-          },
-          params: {
-            has_arg: true,
-            aliases: ['p', 'params']
-          },
-          hierafile: {
-            has_arg: true,
-            aliases: ['h', 'hierafile']
-          },
-          basepath: {
-            has_arg: true,
-            aliases: ['b', 'basepath']
-          },
-          encpath: {
-            has_arg: true,
-            aliases: ['e', 'encpath']
-          },
           version: {
             has_arg: false,
             aliases: ['v', 'version']
-          },
-          yaml_facts: {
-            has_arg: true,
-            aliases: ['y', 'yaml']
-          },
-          json_facts: {
-            has_arg: true,
-            aliases: ['j', 'json']
-          },
-          interactive: {
-            has_arg: false,
-            aliases: ['i', 'interactive']
-          },
-          db: {
-            has_arg: false,
-            aliases: ['db']
-          },
-          nodb: {
-            has_arg: false,
-            aliases: ['nodb', 'no-db', 'no']
           }
         }
       end
