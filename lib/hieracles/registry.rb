914add5de8fce44e0df0ecec03202b33c8dd4411
mose <mose@gandi.net>
2015-12-18 13:21:19 +0800

    unmemoize registries

--- lib/hieracles/registry.rb
+++ lib/hieracles/registry.rb
@@ -4,17 +4,17 @@ module Hieracles
   	extend self
 
   	def farms(config)
-  		@_farms ||= Dir.glob(format(config.classpath, '*')).sort
+  		Dir.glob(format(config.classpath, '*')).sort
   	end
 
   	def nodes(config)
-  		@_nodes ||= Dir.glob(File.join(config.encpath, '*.yaml')).sort.map do |f|
+  		Dir.glob(File.join(config.encpath, '*.yaml')).sort.map do |f|
   			File.basename(f, '.yaml')
   		end
   	end
 
   	def modules(config)
-  		@_modules ||= Dir.glob(File.join(config.modulepath, '*')).sort.map do |f|
+  		Dir.glob(File.join(config.modulepath, '*')).sort.map do |f|
   			File.basename(f)
   		end
   	end
